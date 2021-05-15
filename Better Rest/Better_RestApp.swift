29c741335bf3d065036fe170c88f277c6e4bc780
Gayan Kalinga <bg15407@gmail.com>
2021-05-15 09:14:53 +0530

    Initial Commit

---
+++ Better Rest/Better_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Better_RestApp.swift
+//  Better Rest
+//
+//  Created by Gayan Kalinga on 5/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Better_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
